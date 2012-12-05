69bd14b713f3bd108430bba00b3efef78f854909
404 <404@example.com>
2012-12-05 17:01:25 +0800

    restrucutre

--- recursive_rename/r_rename.rb
+++ recursive_rename/r_rename.rb
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+require_relative './lib/sanitize_title.rb'
 require 'fileutils'
 require 'minitest/autorun'
 
@@ -11,25 +12,6 @@ $input = File.expand_path(ARGV[0].chomp('/'))
 # 我只会用这种笨方法 ^_^
 $char_count_of_higher_path = $input.size
 $output = File.expand_path(ARGV[1].chomp('/'))
-def sanitize(title)
-  # 不要替换 / 否则路径分割符号也被替换了
-  # 因为下面的file是含有路径的
-  # 也不要替换英文句号，因为作为文件后缀分割符号
-  # 例子： "source//TLF/PBS自由选择/PBS.自由选择_2_/_PBS自由选择_PBS.Free_to_Choose_1990.Vol.4"
-  title = title.tr(' `~!@#$%^&*()_+=\|][{}"\';:>,<-', '_')
-  title = title.tr('？ －·～！@#￥%……&*（）——+、|】』【『‘“”；：。》，《', '_')
-  title = title.gsub(/_+/, '_').gsub(/^_/, '').gsub(/_$/, '') # 对开头，结尾和多个 _ 做处理
-  title = title.gsub('_/', '/') #如果路径分割前后有下划线也不要
-  title = title.gsub('/_', '/')
-  # 不知何时引入了多个连续的 // 可这 连续的 // 给我伤害
-  # 哦，我知道何时引入的了，是ARGV[0]没有chomp掉目录的结尾 /
-  title = title.gsub(/\/\/+/, '/')
-  # 文件后缀前的下划线不要 text_.txt不对，应该是text.txt
-  title = title.gsub(/_\./, '.')
-  title = title.downcase
-#  title = title.gsub(/srt$/i,'srt' ) # 为了后面匹配文件名后缀方便，都换成小写
-#  title = title.gsub(/ass$/i,'ass' )
-end
 def recursive_rename(input, output)
   input = $input
   output = $output
@@ -49,20 +31,3 @@ def recursive_rename(input, output)
   end  
 end
 recursive_rename($input, $output)
-
-# begiin tests
-# 现在每次运行这个脚本都会test一下
-# 单独test还需要加上两个假的ARGV参数 ruby script.rb a b
-class TestSanitize < MiniTest::Unit::TestCase
-
-INPUT = '/home/WTF/got-me/input'
-SIZE = INPUT.size
-FILENAME = '/home/WTF/got-me/input/some-file_With-8*&^.txt'
-  def test_sanitize
-    assert_equal "this_is_a_title", sanitize('this is-a title ')
-    assert_equal "经常_有_中_文_符_号", sanitize('经常@!有#中$文%&符*（号）')
-    assert_equal "开头结尾有很多空格", sanitize('  开头结尾有很多空格   ')
-    assert_equal "/home/WTF/got-me/input/some_file_with_8.txt", INPUT + sanitize(FILENAME[SIZE..-1]), 'FIXME: do not sanitize root folder!'
-  end
-
-end
--- recursive_rename/test/test_recursive_rename.rb
+++ recursive_rename/test/test_recursive_rename.rb
@@ -1,5 +1,19 @@
+# -*- coding: utf-8 -*-
+require_relative '../lib/sanitize_title.rb'
 require 'minitest/autorun'
 
-class RecursiveRename < MiniTest::Unit::TestCase
+$input = 'test'
+$output = 'test'
+class TestSanitize < MiniTest::Unit::TestCase
+  INPUT = '/home/WTF/got-me/input'
+  SIZE = INPUT.size
+  FILENAME = '/home/WTF/got-me/input/some-file_With-8*&^.txt'
+  def test_sanitize
+    assert_equal "this_is_a_title", sanitize('this is-a title ')
+    assert_equal "经常_有_中_文_符_号", sanitize('经常@!有#中$文%&符*（号）')
+    assert_equal "开头结尾有很多空格", sanitize('  开头结尾有很多空格   ')
+    assert_equal "/home/WTF/got-me/input/some_file_with_8.txt", INPUT + sanitize(FILENAME[SIZE..-1]), 'FIXME: do not sanitize root folder!'
+  end
+end
 
-  def 
+
